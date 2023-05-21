31a02aac7529fa77e44d447298a24f301586228e
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-05-21 14:17:44 -0300

    study: Aula 04 - Integrando os projetos web e mobile.

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://11.0.0.100:3333',
+  baseURL: 'http://11.0.0.104:3333',
 })
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,28 +1,5 @@
-import React from 'react'
-import { cookies } from 'next/headers'
-import { Copyright } from '@/components/Copyright'
-import { Hero } from '@/components/Hero'
-import { SignIn } from '@/components/SignIn'
 import { EmptyMemories } from '@/components/EmptyMemories'
-import { Profile } from '@/components/Profile'
 
 export default function Home() {
-  const isAuthenticaded = cookies().has('token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 pt-16">
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
-
-        {isAuthenticaded ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-
-      <div className="flex flex-col p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
